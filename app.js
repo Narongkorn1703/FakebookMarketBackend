0e57e1ab16c2de4b0afb695060b08c9a0feb40b5
GuboyChatt <guboychatt@gmail.com>
2021-05-13 17:15:17 +0700

    ready to share episode 2

---
+++ app.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+require("./middlewares/passport");
+const express = require("express");
+const app = express();
+const { PORT } = process.env;
+const { sequelize } = require("./models");
+const userRoute = require("./routes/userRoute");
+const productRoute = require("./routes/productRoute");
+const followerRoute = require("./routes/followerRoute");
+const savedRoute = require("./routes/savedRoute");
+const ratingRoute = require("./routes/ratingRoute");
+const cors = require("cors");
+const error = require("./middlewares/error");
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(userRoute);
+app.use("/product", productRoute);
+app.use("/follower", followerRoute);
+app.use("/saved", savedRoute);
+app.use("/rating", ratingRoute);
+app.use((req, res) => {
+  res.status(404).json({ message: "path not found on this server" });
+});
+app.use(error);
+// sequelize.sync({ alter: true }).then(() => console.log("DB Sync"));
+app.listen(PORT, () => console.log(`This server is running in ${PORT}`));
